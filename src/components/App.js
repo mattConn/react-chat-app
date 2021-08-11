f327afab4d5b5fac2e7a781501ca0b686811ee00
Matthew Connelly <matthew.b.connelly@gmail.com>
2021-08-11 16:05:21 -0400

    generating anon user ID

--- src/components/App.js
+++ src/components/App.js
@@ -3,20 +3,35 @@ import Message from './message'
 
 class App extends React.Component {
   state = {
-    messages: []
+    messages: [],
+    users: {}
   }
 
   inputRef = React.createRef()
 
+  constructor(props) {
+    super(props)
+    let userID = localStorage.getItem('userID')
+
+    if (!userID) {
+      userID = 'Anonymous' + Date.now().toString()
+      localStorage.setItem('userID', userID)
+      const users = { ...this.state.users }
+      users[userID] = userID
+      this.setState({ users: users })
+    }
+  }
+
   sendMessage = (e) => {
     e.preventDefault()
+    const newMessage = {
+      senderName: this.state.users[localStorage.getItem('userID')],
+      timestamp: 'now',
+      content: this.inputRef.current.value,
+      key: Date.now()
+    }
     this.setState({
-      messages: [...this.state.messages,
-      {
-        senderID: 'Matt',
-        timestamp: 'now',
-        content: this.inputRef.current.value
-      }]
+      messages: [...this.state.messages, newMessage]
     })
     e.target.reset()
   }
@@ -27,15 +42,16 @@ class App extends React.Component {
         <div className="messages">
           {this.state.messages.map((msg) => (
             <Message
-              senderID={msg.senderID}
+              senderName={msg.senderName}
               timestamp={msg.timestamp}
               content={msg.content}
+              key={msg.key}
             />
           ))}
         </div>
 
         <form className="messageInput" onSubmit={this.sendMessage}>
-          <input type="text" ref = {this.inputRef} required />
+          <input type="text" ref={this.inputRef} required />
           <button type="submit">Send</button>
         </form>
       </div>
--- src/components/message.js
+++ src/components/message.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Message = (props) => (
     <div className="message" style={{border: "solid 1px"}}>
-        <p>{props.senderID}</p>
+        <p>{props.senderName}</p>
         <p>{props.timestamp}</p>
         <p>{props.content}</p>
     </div>
